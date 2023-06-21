8503e81cac2580a6920b4689a925cbfeca1c26e1
jeremili <prevostjeremy@gmail.com>
2023-06-21 17:45:05 +0200

    Modif index.html

--- app/index.php
+++ app/index.php
@@ -3,8 +3,42 @@
 	<title></title>
 </head>
 <body>
+<table align="center" border="0" cellpadding="10" cellspacing="10" style="background:rgb(68,179,133);width:100%;">
+	<tbody>
+		<tr>
+			<td>
+			<div style="text-align:center;"><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;"><span style="color:#FFFFFF;">A quoi sert mon don ?</span></span></span><br />
+			&nbsp;</div>
 
-Mon super test.
+			<div style="text-align:center;"><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;"><span style="color:#FFFFFF;"><strong>L&#39;Eglise catholique ne re&ccedil;oit ni subvention de l&#39;Etat, ni financement du Vatican.</strong></span></span></span></div>
 
+			<div style="text-align:center;"><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;"><span style="color:#FFFFFF;">Votre don sert &agrave; financer :</span></span></span></div>
+
+			<ul>
+				<li style="color:#FFFFFF;"><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;"><span style="color:#FFFFFF;">les activit&eacute;s pastorales (aum&ocirc;neries, p&eacute;lerinages, sacrements...)</span></span></span></li>
+				<li style="color:#FFFFFF;"><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;"><span style="color:#FFFFFF;">la contribution aux conditions de vie de nos pr&ecirc;tres</span></span></span></li>
+				<li style="color:#FFFFFF;"><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;"><span style="color:#FFFFFF;">la r&eacute;mun&eacute;ration des la&iuml;cs au service de l&#39;Eglise</span></span></span></li>
+				<li style="color:#FFFFFF;"><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;"><span style="color:#FFFFFF;">l&#39;entretien de nos &eacute;glises</span></span></span></li>
+			</ul>
+			</td>
+			<td>
+			<h4 style="text-align:center;"><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;"><span style="color:#FFFFFF;">Respect de votre vie priv&eacute;e</span></span></span></h4>
+
+			<div><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;"><span style="color:#FFFFFF;">Dans le cadre de la RGPD, vous disposez d&rsquo;un droit d&rsquo;acc&egrave;s, de modification, de rectification et de suppression de vos donn&eacute;es, conform&eacute;ment &agrave; notre</span><a href="https://www.catholique78.fr/politique-de-confidentialite/"><span style="color:#FFFFFF;">Politique de confidentialit&eacute;</span></a><span style="color:#FFFFFF;">.<br />
+			Ces droits peuvent &ecirc;tre exerc&eacute;s, par courrier &eacute;lectronique &agrave; donateurs@catholique78.fr, par t&eacute;l&eacute;phone 01 30 97 67 78 ou par courrier &agrave; l&rsquo;attention de Service donateurs / Association dioc&eacute;saine de Versailles, 16 rue Mgr Gibier 78000 Versailles.<br />
+			Pour plus d&rsquo;informations, nous vous invitons &agrave; vous adresser &agrave; notre D&eacute;l&eacute;gu&eacute; &agrave; la protection des donn&eacute;es<br />
+			&agrave; dpo@catholique78.fr.<br />
+			<br />
+			<strong>Le dioc&egrave;se de Versailles s&#39;engage &agrave; ne pas communiquer vos coordonn&eacute;es.</strong></span></span></span></div>
+			</td>
+			<td>
+			<div style="text-align:center;"><strong><span style="color:#FFFFFF;"><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;">Donnez en toute confiance</span></span></span></strong></div>
+			<br />
+			<span style="color:#FFFFFF;"><span style="font-size:12px;"><span style="font-family:tahoma, geneva, sans-serif;">Toutes vos informations bancaires pour traiter votre paiement sont crypt&eacute;es gr&acirc;ce au protocole SSL. <strong>Elles ne sont en aucun cas conserv&eacute;es sur nos syst&egrave;mes informatiques. </strong>Notre service donateurs est &agrave; votre disposition pour r&eacute;pondre &agrave; toutes vos questions.<br />
+			<strong>Pour nous contacter :</strong><br />
+			Association Dioc&eacute;saine de Versailles Service donateurs 16 rue Monseigneur Gibier 78000 Versailles T&eacute;l : 01 30 97 67 78 E-mail :donateurs@catholique78.fr</span></span></span></td>
+		</tr>
+	</tbody>
+</table>
 </body>
 </html>
